52d2631a5b8cfbc2c1946feac97325b2e6691b23
Tyler Ellingen <Tyler.Ellingen@milliman.com>
2022-11-07 16:17:51 -0600

    Add project files.

---
+++ IronPdf.BugRepro/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Text;
+using System;
+using IronPdf;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        GeneratePDF();
+    }
+    static void GeneratePDF()
+    {
+        StringBuilder sb = new StringBuilder();
+        sb.AppendLine(@"<HTML><BODY style='color: blue;'>Hello World</BODY></HTML>");
+        var Renderer = new ChromePdfRenderer();
+        Renderer.RenderingOptions.PaperSize = IronPdf.Rendering.PdfPaperSize.Letter;
+        Renderer.RenderingOptions.GrayScale = true;
+        PdfDocument PDF = Renderer.RenderHtmlAsPdf(sb.ToString());
+        PDF.SaveAs(@$"{Environment.CurrentDirectory}\{Guid.NewGuid()}.pdf");
+    }
+}
